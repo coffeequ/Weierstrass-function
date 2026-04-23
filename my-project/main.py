ec347370346a00cbf14c8b5f3fa4c05be442a727
coffeequ <savvapugach2004@mail.ru>
2026-04-23 19:24:13 +0500

    add axis scen

--- my-project/main.py
+++ my-project/main.py
@@ -1,13 +1,9 @@
 from manim import *
 
-
-class CreateCircle(Scene):
+class WeierstrassFun(ThreeDScene):
     def construct(self):
-        circle = Circle()  # create a circle
-        square = Square()
+        axes = Axes().animate
+        self.add(axes)
+
+        self.play()
         
-        square.set_fill(RED, opacity=1)
-        
-        circle.set_fill(PINK, opacity=1)  # set the color and transparency
-        
-        self.play(Create(square), Create(circle))
